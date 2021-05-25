80a559b96d8892cacb81bfb7e927e26359dc0bc7
Rob Dockins <rdockins@galois.com>
2021-05-25 15:48:56 -0700

    Add a new `default_term` command that applies Cryptol defaulting rules to a SAWCore term.

--- src/SAWScript/Interpreter.hs
+++ src/SAWScript/Interpreter.hs
@@ -908,6 +908,11 @@ primitives = Map.fromList
     [ "Docs TODO!!"
     ]
 
+  , prim "default_term" "Term -> Term"
+    (funVal1 default_typed_term)
+    Experimental
+    [ "Apply Cryptol defaulting rules to the given term." ]
+
   , prim "sbv_uninterpreted"   "String -> Term -> TopLevel Uninterp"
     (pureVal sbvUninterpreted)
     Deprecated
